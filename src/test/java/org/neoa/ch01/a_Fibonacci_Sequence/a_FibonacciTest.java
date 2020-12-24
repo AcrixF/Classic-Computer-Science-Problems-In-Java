4c7ea3a9845a1cabf88c8c9d7b76ff81ca06f440
Cristian Flores Martínez <acrixf@gmail.com>
2020-12-23 22:54:27 -0600

    define Fibonacci class

--- src/test/java/org/neoa/ch01/a_Fibonacci_Sequence/a_FibonacciTest.java
+++ src/test/java/org/neoa/ch01/a_Fibonacci_Sequence/a_FibonacciTest.java
@@ -1,2 +1,47 @@
-package org.neoa.ch01.a_Fibonacci_Sequence;public class a_FibonacciTest {
+package org.neoa.ch01.a_Fibonacci_Sequence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.neoa.ch01.a_Fibonacci_Sequence.Fibonacci.fibonacci;
+
+public class a_FibonacciTest {
+
+    private List<Integer> sequence = null;
+
+    @BeforeEach
+    void setup() {
+        sequence = new ArrayList<>();
+    }
+
+    @Test
+    void fibonacciReturnsEmptySequenceWhenIndexIsZero() {
+        for (int i = 0; i < 0; i++)
+            sequence.add(fibonacci(i));
+
+        assertThat(sequence).isEmpty();
+    }
+
+    @Test
+    void fibonacciReturnSequenceWithOnlyOneElementWhenIndexIsEqualToOne() {
+        for (int i = 0; i < 1; i++)
+            sequence.add(fibonacci(i));
+
+        assertThat(sequence)
+                .hasSize(1)
+                .containsExactlyInAnyOrder(0);
+    }
+
+    @Test
+    void fibonacciReturnsExpectedSequenceWhenIndexIsGreaterThanTwo() {
+        for (int i = 0; i < 5; i++)
+            sequence.add(fibonacci(i));
+
+        assertThat(sequence)
+                .containsExactlyInAnyOrder(0,1, 1, 2, 3);
+    }
 }
