f42e5bef095a59eb2cd368c44358b5f51482b6d8
Cristian Flores Martínez <acrixf@gmail.com>
2020-12-23 23:24:39 -0600

    add fibonacci memoization version

--- src/main/java/org/neoa/ch01/a_Fibonacci_Sequence/FibonacciMemoization.java
+++ src/main/java/org/neoa/ch01/a_Fibonacci_Sequence/FibonacciMemoization.java
@@ -1,2 +1,17 @@
-package org.neoa.ch01.a_Fibonacci_Sequence;public class FibonacciMemoization {
+package org.neoa.ch01.a_Fibonacci_Sequence;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FibonacciMemoization {
+
+    private static Map<Integer, Integer> memo = new HashMap<>(Map.of(0,0,1,1));
+
+    public static int fibonacci(int n) {
+        if (!memo.containsKey(n)) {
+            memo.put(n, fibonacci(n - 1) + fibonacci(n - 2));
+        }
+        return memo.get(n);
+    }
+
 }
--- src/test/java/org/neoa/ch01/a_Fibonacci_Sequence/FibonacciMemoizationTest.java
+++ src/test/java/org/neoa/ch01/a_Fibonacci_Sequence/FibonacciMemoizationTest.java
@@ -1,2 +1,40 @@
-package org.neoa.ch01.a_Fibonacci_Sequence;public class FibonacciMemoizationTest {
+package org.neoa.ch01.a_Fibonacci_Sequence;
+
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.neoa.ch01.a_Fibonacci_Sequence.FibonacciMemoization.fibonacci;
+
+public class FibonacciMemoizationTest {
+
+    private List<Integer> sequence = null;
+
+    @BeforeEach
+    void setup() {
+        sequence = new ArrayList<>();
+    }
+
+    @Test
+    void fibonacciReturnsExpectedSequenceWhenIndexIsGreaterThanTwo() {
+        for (int i = 0; i < 5; i++)
+            sequence.add(fibonacci(i));
+
+        assertThat(sequence)
+                .containsExactlyInAnyOrder(0,1, 1, 2, 3);
+    }
+
+    @Test
+    void fibonacciReturnsExpectedSequenceWhenNIsGreaterThan50() {
+        int n = 50;
+        for (int i = 0; i < n; i++)
+            sequence.add(fibonacci(i));
+
+        Approvals.verify(sequence);
+    }
+
 }
